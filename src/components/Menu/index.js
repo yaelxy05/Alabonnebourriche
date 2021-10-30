b8688a76235a55624cd0be30021c56ab4c3dcc6c
yaelx <yaelx@localhost>
2021-10-30 22:01:51 +0200

    add responsive mobile and tablet

---
+++ src/components/Menu/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+// == Import components
+
+
+// == Import
+import './menu.scss';
+// == Import img
+
+// == Composant
+const Menu = () => (
+  <div className="register">
+    <h1>Notre carte</h1>
+  </div>
+);
+
+// == Export
+export default Menu;
