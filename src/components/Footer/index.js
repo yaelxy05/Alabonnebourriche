807411d3d1c43858f85a1f051f43d0a25a66bc2b
yaelx <yaelx@localhost>
2021-10-30 22:40:57 +0200

    add responsive footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,19 +10,21 @@ const Footer = () => (
   <div className="footer">
     <div className="footer_wrapper">
       <img src={logo} alt="logo a la bonne bourriche" />
-      <div className="footer_horaire">
-        <h3>Horaires</h3>
-        <p>Lun : 12h00 - 15h00 / 19h00 - 22h00</p>
-        <p>Lun : 12h00 - 15h00 / 19h00 - 22h00</p>
-        <p>Lun : 12h00 - 15h00 / 19h00 - 22h00</p>
-        <p>Lun : 12h00 - 15h00 / 19h00 - 22h00</p>
-        <p>Lun : 12h00 - 15h00 / 19h00 - 22h00</p>
-        <p>Lun : 12h00 - 15h00 / 19h00 - 22h00</p>
-        <p>Lun : 12h00 - 15h00 / 19h00 - 22h00</p>
-      </div>
-      <div className="footer_address">
-        <h3>Adresse</h3>
-        <p>9 rue des pêcheurs de moules <br/>13202 Cassis</p>
+      <div className="footer_info">
+        <div className="footer_horaire">
+          <h3>Horaires</h3>
+          <p>Du lundi au samedi de 12h à 14h et de 19h30 à 23h00</p>
+
+        </div>
+        <div className="footer_address">
+          <h3>Adresse</h3>
+          <p>
+            9 rue des pêcheurs de moules <br />
+            13202 Cassis
+            <br />
+            Téléphone : 04.92.69.54.36
+          </p>
+        </div>
       </div>
     </div>
   </div>
